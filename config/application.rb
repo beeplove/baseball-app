69932e9b4db8c042b55364605985e6e04f98e095
Mohammad Khan <mohammad.khan@gmx.us>
2013-09-05 15:44:08 -0700

    asset managerment

--- config/application.rb
+++ config/application.rb
@@ -57,6 +57,6 @@ module BaseballApp
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    config.assets.version = '20130905.1600'
   end
 end
